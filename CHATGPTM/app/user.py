a7e2755dc992c242f06fd2436d9e6fc8f55626dd
Stellaris57 <baskakoviv57@gmail.com>
2024-12-07 21:33:49 +0300

    Update user.py

--- CHATGPTM/app/user.py
+++ CHATGPTM/app/user.py
@@ -1,6 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram import Router, F, types, Dispatcher
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
@@ -8,35 +6,38 @@ from asyncio import sleep
 from app.database.requests import set_user
 # from middlewares import BaseMiddleware
 from app.generators import generate
-
 from app.states import Work
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 user = Router()
-
 # user.message.middleware(BaseMiddleware())
 
 @user.message(CommandStart())
 async def cmd_start(message: Message):
     await set_user(message.from_user.id)
-    await message.answer('Добро пожаловать в бот!')
 
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text = "Модель 1", callback_data="button1"),
+        ],
+        [
+            InlineKeyboardButton(text = "Модель 2", callback_data="button2"),
+        ],
+        [
+            InlineKeyboardButton(text = "Модель 3", callback_data="button3"),
+        ]
+    ])
 
-"""keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text = "Модель 1", callback_data = "pixtral-large-latest")]
-    ]
-)
-model_name = callback_data"""
+    await message.answer('Добро пожаловать в бот!', reply_markup=keyboard)
+
 @user.message(Work.process)
 async def stop(message: Message):
     await message.answer("Подождите, ваш запрос ещё обрабатывается!")
-
 
 @user.message()
 async def ai(message: Message, state: FSMContext):
     await state.set_state(Work.process)
     processing_message = await message.answer("GPT обрабатывает ваш запрос, пожалуйста, подождите...")
-
     res = await generate(message.text)
     await sleep(1)
     #print(res.choices[0].message.content)
